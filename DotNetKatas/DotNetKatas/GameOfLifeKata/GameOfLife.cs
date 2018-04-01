27c04729faca2676cbba07f0f7270f013a1374fc
Simon Jones <contact@simonpj.co.uk>
2018-04-01 18:09:44 +0100

    Implemented game of life kata

---
+++ DotNetKatas/DotNetKatas/GameOfLifeKata/GameOfLife.cs
@@ -0,0 +1,46 @@
+﻿namespace DotNetKatas.GameOfLifeKata
+{
+    class GameOfLife
+    {
+        internal static char[,] NextGeneration(char[,] input)
+        {
+            var nextGen = new char[input.GetLength(0), input.GetLength(1)];
+
+            for (var i = 0; i < input.GetLength(0); i++)
+            {
+                for (var j = 0; j < input.GetLength(1); j++)
+                {
+                    var neighbours = NeighbourCount(input, i, j);
+
+                    if (neighbours == 3)
+                        nextGen[i, j] = '*';
+                    else if (neighbours > 3 || neighbours < 2)
+                        nextGen[i, j] = '.';
+                    else
+                        nextGen[i, j] = input[i, j];
+                }
+            }
+
+            return nextGen;
+        }
+
+        private static int NeighbourCount(char[,] input, int i, int j)
+        {
+            int count = 0;
+
+            if (j - 1 >= 0 && input[i, j - 1] == '*')
+                count++;
+
+            if (i - 1 >= 0 && input[i - 1, j] == '*')
+                count++;
+
+            if (j + 1 < input.GetLength(1) && input[i, j + 1] == '*')
+                count++;
+
+            if (i + 1 < input.GetLength(0) && input[i + 1, j] == '*')
+                count++;
+
+            return count;
+        }
+    }
+}
---
+++ DotNetKatas/DotNetKatas/GameOfLifeKata/GameOfLifeTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace DotNetKatas.GameOfLifeKata
+{
+    public class GameOfLifeTests
+    {
+        /// <summary>
+        /// Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+        /// </summary>
+        [Theory]
+        [MemberData(nameof(RuleOneInputs))]
+        public void RuleOneIsSatisfied(char[,] input, char[,] expectedOutput)
+        {
+            Assert.True(ArraysEqual(expectedOutput, GameOfLife.NextGeneration(input)));
+        }
+
+        /// <summary>
+        /// Any live cell with more than three live neighbours dies, as if by overcrowding.
+        /// </summary>
+        [Theory]
+        [MemberData(nameof(RuleTwoInputs))]
+        public void RuleTwoIsSatisfied(char[,] input, char[,] expectedOutput)
+        {
+            Assert.True(ArraysEqual(expectedOutput, GameOfLife.NextGeneration(input)));
+        }
+
+        /// <summary>
+        /// Any live cell with two or three live neighbours lives on to the next generation.
+        /// </summary>
+        [Theory]
+        [MemberData(nameof(RuleThreeInputs))]
+        public void RuleThreeIsSatisfied(char[,] input, char[,] expectedOutput)
+        {
+            Assert.True(ArraysEqual(expectedOutput, GameOfLife.NextGeneration(input)));
+        }
+
+        /// <summary>
+        /// Any dead cell with exactly three live neighbours becomes a live cell
+        /// </summary>
+        [Theory]
+        [MemberData(nameof(RuleFourInputs))]
+        public void RuleFourIsSatisfied(char[,] input, char[,] expectedOutput)
+        {
+            Assert.True(ArraysEqual(expectedOutput, GameOfLife.NextGeneration(input)));
+        }
+
+        private bool ArraysEqual(char[,] expected, char[,] actual)
+        {
+            if (expected.Length != actual.Length && expected.LongLength != actual.LongLength)
+                return false;
+
+            for (var i = 0; i < expected.GetLength(0); i++)
+            {
+                for (var j = 0; j < expected.GetLength(1); j++)
+                {
+                    if (expected[i, j] != actual[i, j])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static IEnumerable<object[]> RuleOneInputs =>
+            new List<object[]> {
+                new object[] { new char[,] { { '*' }, { '.' } }, new char[,] { { '.' }, { '.' } } },
+                new object[] { new char[,] { { '.', '.', '.' }, { '.', '*', '.' }, { '.', '.', '.' } }, new char[,] { { '.', '.', '.' }, { '.', '.', '.' }, { '.', '.', '.' } } },
+                new object[] { new char[,] { { '.', '.', '.' }, { '.', '*', '.' }, { '.', '.', '.' } }, new char[,] { { '.', '.', '.' }, { '.', '.', '.' }, { '.', '.', '.' } } },
+                new object[] { new char[,] { { '.', '*', '.' }, { '.', '*', '.' }, { '.', '.', '.' } }, new char[,] { { '.', '.', '.' }, { '.', '.', '.' }, { '.', '.', '.' } } }
+            };
+
+        public static IEnumerable<object[]> RuleTwoInputs =>
+            new List<object[]> {
+                new object[] { new char[,] { { '.', '*', '.' }, { '*', '*', '*' }, { '.', '*', '.' } }, new char[,] { { '.', '.', '.' }, { '.', '.', '.' }, { '.', '.', '.' } } }
+            };
+
+        public static IEnumerable<object[]> RuleThreeInputs =>
+           new List<object[]> {
+                new object[] { new char[,] { { '*', '*', '*' } }, new char[,] { { '.', '*', '.' } } },
+                new object[] { new char[,] { { '*', '*', '*' }, { '.', '*', '.' }}, new char[,] { { '.', '*', '.' }, { '.', '.', '.' } } }
+           };
+
+        public static IEnumerable<object[]> RuleFourInputs =>
+            new List<object[]> {
+                new object[] { new char[,] { { '.', '*', '.' }, { '*', '.', '*' }}, new char[,] { { '.', '.', '.' }, { '.', '*', '.' } } }
+            };
+    }
+}
