ba7ae998e4da56c48657916641b9ce6ad7552de5
Simon Jones <contact@simonpj.co.uk>
2018-04-02 21:49:42 +0100

    Started working on alphabet cipher in f#

---
+++ DotNetKatas/FSharpKatas/AlphabetCipherKata/AlphabetCipher.fs
@@ -0,0 +1,25 @@
+﻿module AlphabetCipher
+
+let GetIntersect (inputChar: char) (keywordChar: char) =
+    let inputCharInt = (int inputChar) - 97
+    let keywordCharInt = (int keywordChar) - 97
+
+    let sum = inputCharInt + keywordCharInt
+
+    if sum >= 26 then
+        let adjustedSum = sum - 26 + 97
+        (char adjustedSum)
+    else
+        let adjustedSum = sum + 97
+        (char adjustedSum)
+        
+let RepeatKeyword (input: string) (keyword: string) = 
+    let multiplier = (String.length input / String.length keyword) + 1
+    let repeated = String.replicate multiplier keyword
+    repeated.[0..String.length input - 1]
+
+let Encode input keyword = 
+    RepeatKeyword input keyword 
+    |> Seq.zip input
+    |> Seq.map (fun (x, y) -> GetIntersect x y) 
+    |> System.String.Concat
---
+++ DotNetKatas/FSharpKatas/AlphabetCipherKata/AlphabetCipherTests.fs
@@ -0,0 +1,26 @@
+﻿module AlphabetCipherTests
+
+open Xunit
+open AlphabetCipher
+
+[<Theory>]
+[<InlineData("s", "m", "e")>]
+[<InlineData("q", "d", "t")>]
+[<InlineData("f", "f", "k")>]
+[<InlineData("a", "a", "a")>]
+[<InlineData("z", "z", "y")>]
+[<InlineData("y", "c", "a")>]
+let ``Intersect returns the correct value`` x y expected =
+    Assert.Equal(expected, GetIntersect x y)
+
+[<Theory>]
+[<InlineData("afairlylonginput", "keyword", "keywordkeywordke")>]
+[<InlineData("samelength", "samelength", "samelength")>]
+let ``Repeat keyword gives then correct result`` input keyword expected =
+    Assert.Equal(expected, RepeatKeyword input keyword)
+
+[<Theory>]
+[<InlineData("meetmeontuesdayeveningatseven", "vigilance", "hmkbxebpxpmyllyrxiiqtoltfgzzv")>]
+[<InlineData("meetmebythetree", "scones", "egsgqwtahuiljgs")>]
+let ``Can encode given a secret keyword`` input keyword expected = 
+    Assert.Equal(expected, Encode input keyword)
