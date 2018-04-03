916b21d8ce993c1cd24ee868c87cae910c405f2d
Simon Jones <contact@simonpj.co.uk>
2018-04-03 21:40:13 +0100

    Implemented decoding function. Working on decipher

--- DotNetKatas/FSharpKatas/AlphabetCipherKata/AlphabetCipher.fs
+++ DotNetKatas/FSharpKatas/AlphabetCipherKata/AlphabetCipher.fs
@@ -12,7 +12,27 @@ let GetIntersect (inputChar: char) (keywordChar: char) =
     else
         let adjustedSum = sum + 97
         (char adjustedSum)
-        
+
+let DecodeChar (inputChar: char) (keywordChar: char) =
+    let sum = (int inputChar) - 97
+    let inputCharInt = sum - (int keywordChar) + 194
+
+    if inputCharInt < 97 then
+        let adjusted = inputCharInt + 26
+        (char adjusted)
+    else 
+        (char inputCharInt)
+
+let DecipherChar (inputChar: char) (outputChar: char) =
+    let sum = (int outputChar) - 97
+    let keywordCharInt = sum - (int inputChar) + 194
+
+    if keywordCharInt < 97 then
+        let adjusted = keywordCharInt + 26
+        (char adjusted)
+    else 
+        (char keywordCharInt)
+     
 let RepeatKeyword (input: string) (keyword: string) = 
     let multiplier = (String.length input / String.length keyword) + 1
     let repeated = String.replicate multiplier keyword
@@ -22,4 +42,16 @@ let Encode input keyword =
     RepeatKeyword input keyword 
     |> Seq.zip input
     |> Seq.map (fun (x, y) -> GetIntersect x y) 
+    |> System.String.Concat
+
+let Decode input keyword = 
+    RepeatKeyword input keyword
+    |> Seq.zip input
+    |> Seq.map (fun (x, y) -> DecodeChar x y)
+    |> System.String.Concat
+
+let Decipher input output = 
+    input
+    |> Seq.zip output
+    |> Seq.map (fun (x, y) -> DecipherChar x y)
     |> System.String.Concat
--- DotNetKatas/FSharpKatas/AlphabetCipherKata/AlphabetCipherTests.fs
+++ DotNetKatas/FSharpKatas/AlphabetCipherKata/AlphabetCipherTests.fs
@@ -24,3 +24,34 @@ let ``Repeat keyword gives then correct result`` input keyword expected =
 [<InlineData("meetmebythetree", "scones", "egsgqwtahuiljgs")>]
 let ``Can encode given a secret keyword`` input keyword expected = 
     Assert.Equal(expected, Encode input keyword)
+
+[<Theory>]
+[<InlineData("a", "c", "y")>]
+[<InlineData("y", "z", "z")>]
+[<InlineData("a", "a", "a")>]
+[<InlineData("e", "m", "s")>]
+[<InlineData("t", "d", "q")>]
+[<InlineData("k", "f", "f")>]
+let ``DecodeChar give the correct result`` input keyword expected =
+    Assert.Equal(expected, DecodeChar input keyword)
+
+[<Theory>]
+[<InlineData("hmkbxebpxpmyllyrxiiqtoltfgzzv", "vigilance", "meetmeontuesdayeveningatseven")>]
+[<InlineData("egsgqwtahuiljgs", "scones", "meetmebythetree")>]
+let ``Can decode given a secret keyword`` input keyword expected = 
+    Assert.Equal(expected, Decode input keyword)
+
+[<Theory>]
+[<InlineData("s", "e", "m")>]
+[<InlineData("q", "t", "d")>]
+[<InlineData("f", "k", "f")>]
+[<InlineData("a", "a", "a")>]
+[<InlineData("z", "y", "z")>]
+[<InlineData("y", "a", "c")>]
+let ``DecipherChar gives then correct result`` input output expected =
+    Assert.Equal(expected, DecipherChar input output)
+
+[<Theory>]
+[<InlineData("hmkbxebpxpmyllyrxiiqtoltfgzzv", "meetmeontuesdayeveningatseven", "vigilance")>]
+let ``Can decipher keyword given and input and output`` input output expected = 
+    Assert.Equal(expected, Decipher input output)
